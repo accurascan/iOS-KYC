2e89379785ad8b21d81d0074e9892539f84e4fb4
Kevin <info@technzoer.com>
2021-07-30 17:54:23 +0530

    SDK fixes and MRZ fixes

--- CodeScan/Controllers/UIViewControllers/Resultpdf417ViewController.swift
+++ CodeScan/Controllers/UIViewControllers/Resultpdf417ViewController.swift
@@ -121,6 +121,7 @@ class Resultpdf417ViewController: UIViewController,UITableViewDataSource,UITable
         liveness.setFeedBackGlareFaceMessage("Glare detected")
         liveness.setBlurPercentage(80)
         liveness.setGlarePercentage(-1, -1)
+        liveness.evaluateServerTrustWIthSSLPinning(true)
         
         facematch.setBackGroundColor("#C4C4C5")
         facematch.setCloseIconColor("#000000")
