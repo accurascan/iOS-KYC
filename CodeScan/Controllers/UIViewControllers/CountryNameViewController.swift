e91b5deecaa56f4e6e62f91aa05d50a7eb457569
mahdisomjee123 <90239736+mahdisomjee123@users.noreply.github.com>
2024-08-06 14:26:52 +0530

    Update CountryNameViewController.swift

--- CodeScan/Controllers/UIViewControllers/CountryNameViewController.swift
+++ CodeScan/Controllers/UIViewControllers/CountryNameViewController.swift
@@ -118,6 +118,10 @@ class CountryNameViewController: UIViewController, UITableViewDelegate, UITableV
                 self.accuraCameraWrapper?.setGlarePercentage(6, intMax: 99)
                 self.accuraCameraWrapper?.setBlurPercentage(60)
                 self.accuraCameraWrapper?.setCameraFacing(.CAMERA_FACING_BACK)
+                 self.accuraCameraWrapper?.isApiCallingEnable(true)
+                 self.accuraCameraWrapper?.apiurl("Your URL")
+                 self.accuraCameraWrapper?.setProgressDialogue("API loading...")
+                 self.accuraCameraWrapper?.apiTimer(100.0)
 //                self.accuraCameraWrapper?.setCheckPhotoCopy(false, stCheckPhotoMessage: "")
              }
             }
